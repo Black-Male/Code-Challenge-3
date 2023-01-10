0cc2a540e3aa6f51859d9a16647e11a566f62fd5
Black-Male <Gkaranja20@gmail.com>
2023-01-10 23:40:24 +0300

    Resubmission

--- Java/index.js
+++ Java/index.js
@@ -1,91 +1,74 @@
-let doIt = () => {
-    fetch("http://localhost:3000/films")
-    .then(res => res.json())
-    .then(data => console.log(data))
-}
-doIt()
+// let doIt = () => {
+//     fetch("http://localhost:3000/films")
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+// }
+// doIt()
 
-//used this to make sure that the movie list will automatically appear on the page with no calling 
-document.addEventListener("DOMContentLoaded", () => {
-    //fetch bellow calls for all movies to be listed on the page
-    fetch ("http://localhost:3000/films")
-    .then(res => res.json())
-    .then(data => data.forEach(Data => ListMovies(Data)))
+let movies = []
+let searchInput =  document.querySelector("#srch-bar")
+let film = document.querySelector('.film-card')
 
-    //function/event below is to listen and tells computer what to do after submit has been called
-    // this is to search for a specific movie 
-    document.querySelector('form').addEventListener('submit', (e) => {
-        e.preventDefault()
-        let Inputs = e.target.querySelector("#text-input")
-        let val = Inputs.value
-        fetch(`http://localhost:3000/films?q=${val}`)
-        .then(res => res.json())
-        .then(data => filmDisplay(data))
-    })
-    
-    function filmDisplay(z){
-        if (z.length > 2){
-            warn(`Search Not Specific`)
-        }else {
-            let data = z[0]
-            let insertionPoint = document.querySelector('#filmInfo')
-            let element =  document.createElement('div')
-            console.log(data)
-            let resp;
-            element.innerHTML = `
-            <img src="${data.poster}" alt="MoviePoster">
-            <h1>${data.title}</h1>
-            <h2>${data.description}</h2>
-            <ul>
-                <li>duration: ${data.runtime}</li>
-                <li>air time:${data.showtime}</li>
-                <li>max cinema capacity:${data.capacity}</li>
-                <li id ="tick">current tickets:${data.tickets_sold}</li>
-                <p>${resp}<p>
-                <button id="BookSeat">BookSeat</button>
-            </ul>
-            `
-            insertionPoint.appendChild(element)
+// searchInput.addEventListener("input", z => {
+//     const Value = z.target.value.toLowerCase()
+//     console.log(Value)
+//     movies.forEach(movie => {
+//         console.log (movie)
+//         let isVisible = movie.title.includes(Value)
+//         film.element.classList.toggle("hide",!isVisible)
+//     })
+// })
 
-            element.querySelector(`#BookSeat`).addEventListener('click',() => {
-                if (data.capacity - data.tickets_sold > 0){
-                    data.tickets_sold = data.tickets_sold-1
-                    let y = data.tickets_sold
-                    let resp = "TICKET SOLD"
-                    alert(resp)
-                    alert(`available tickets are now ${y}`)
-                    // element.querySelector('tick').textContent= `${data.tickets_sold}`
-                    // element.querySelector('p').textContent = `${resp}`
-                }else {
-                    let resp = "SOLD OUT!"
-                    // console.warn(`${data.title} IS SOLD OUT!`);
-                    // element.querySelector('p').textContent = `${resp}`
-                }
-            } )
+function CreateFilmCard(z){
+    //let z = a[0]
+    //console.log (a)
+    //console.log(z)
+    let y = document.createElement('div')
+    y.className = "film-card"
+    let insidestuff = `
+    <h1>${z.title}</h1>
+    <img src="${z.poster}" alt="Film Poster">\
+    <p>${z.description}</p>
+    <h2>Runtime: ${z.runtime}</h2>
+    <h3>Showtime: ${z.showtime}:</h3>
+    <p id = "cap">Capacity: ${z.capacity}:</p>
+    <p id = "tick">Tickets sold: ${z.tickets_sold}</p>
+    <p id="ava">Available tickets: ${z.capacity - z.tickets_sold} </p>
+    <button>Book ticket</button>
+
+    `
+    y.innerHTML = insidestuff
+    document.querySelector('.films').appendChild(y)
+
+    let btn = y.querySelector('button')
+    btn.addEventListener('click', () => {
+        let availableTickets = z.capacity - z.tickets_sold
+        if (availableTickets > 0){
+            alert (`Ticket for ${z.title} Bought!`);
+            // let tick = y.querySelector('#tick')
+            // tick.innerHTML =`available tickets: ${z.tickets_sold - 1}`
+            // let cap = y.querySelector('#cap')
+            // cap.innerHTML = `Capacity:`
+            let a = z.tickets_sold
+            a ++
+            let tick = y.querySelector('#tick')
+            tick.innerHTML = `Tickets sold: ${a}`
+            let ava = y.querySelector("#ava")
+            ava.innerHTML = `Available tickets : ${z.capacity - a}`
 
         }
+    })
 
-    }
-})
-//this function here decides how the items are arranged and the order in  which they appear in on html 
-let ListMovies = (z) => {
-    let card = document.createElement('li')
-    card.id = z.id
-    let image = document.createElement('img')
-    image.src = z.poster
-    let filmName = document.createElement('h1')
-    filmName.innerHTML = z.title
-    let description = document.createElement('p')
-    description.innerHTML = z.description
-    let btn = document.createElement('button')
-    btn.textContent = 'Movie-Info'
-    
-    card.appendChild(image)
-    card.appendChild(filmName)
-    card.appendChild(description)
-    card.appendChild(btn)
-
-    let insertionPoint = document.querySelector('ul')
-    insertionPoint.appendChild(card)
 }
+
+document.addEventListener('DOMContentLoaded',() => {
+    fetch("http://localhost:3000/films")
+    .then(res => res.json())
+    .then(data => 
+       movies= data.map(Data => CreateFilmCard(Data)))
+})
+
+
+
+
 
--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 # How it works
 The project allows you to search from the collection of movies on the page so that you can book for the film you wish to watch.
 
-The projects functionality is not optimal as the avilable tickets do not update visibly, however you are alerted on the number of available tickets after each purchase of a ticket
+Once you book a ticket the page will alert you that a ticket has been bought  and will change the number of available tickets and tickets-sold
 
 # Initializing the project
 to get my application you have to clone the repository into the right environment, you do this by entering the terminal and using the commands to access the directory you wish to clone the repository into 
@@ -22,6 +22,11 @@ to view the work you will have to enter the folder by running
         cd phase1-code-challenge
 
 you can then oppen it from the terminal by running code . or manually open it through any other editor you have
+
+The project also needs you to initialize the local json server and this is done by typing 
+
+        npm install -g json-server
+        json server --watch db.json
 
 # Contributors
 
